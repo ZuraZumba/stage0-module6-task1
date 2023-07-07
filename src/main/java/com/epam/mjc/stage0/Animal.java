cb4ff651b3943121ca7269237a40822b7df05d9c
zurazumba <zura.zumbadze@yahoo.com>
2023-07-08 03:02:37 +0400

    oop

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,26 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String furStatus = hasFur ? "a" : "no";
+        String pawText = numberOfPaws == 1 ? "paw" : "paws";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + pawText + " and " + furStatus + " fur.";
+    }
+
+    public static void main (String [] args){
+        Animal bird = new Animal("red", 3, true);
+    }
 
 }
+
+
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,8 @@
 package com.epam.mjc.stage0;
+import com.epam.mjc.stage0.Animal;
+public class Dog extends Animal {
+    public Dog (){
+        super("brown", 4, true);
+    }
+    }
 
-public class Dog {
-
-}
